fd7dd31f849aa6f530234367cf4d01b10dd94eb8
meeboom <natchaya.jaru@gmail.com>
2018-09-12 14:08:33 +0700

    [Created] add ProductsController.java

--- src/main/java/com/toyrnotus/toyrnotusapp/controllers/ProductsController.java
+++ src/main/java/com/toyrnotus/toyrnotusapp/controllers/ProductsController.java
@@ -1,14 +1,22 @@
-package com.toyrnotus.controllers;
+package com.toyrnotus.toyrnotusapp.controllers;
 
-import com.toyrnotus.dtos.SearchProductsResp;
+import com.toyrnotus.toyrnotusapp.dtos.SearchProductsResp;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("products")
 public class ProductsController {
 
-    @RequestMapping()
+    private String age;
+    private String gender;
+
+
+    @RequestMapping(method = RequestMethod.GET)
     public SearchProductsResp searchProducts (@RequestParam("age") String age, @RequestParam("gender") String gender)
     {
-
+        return null;
     }
 }
