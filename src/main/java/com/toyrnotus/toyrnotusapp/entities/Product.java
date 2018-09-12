93e4a1b0911a6ad1436ac59379e5d125a26b68c0
somAmz <som_np@hotmail.com>
2018-09-12 12:04:56 +0700

    [Created]: create product entities file

---
+++ src/main/java/com/toyrnotus/toyrnotusapp/entities/Product.java
@@ -0,0 +1,67 @@
+package com.toyrnotus.toyrnotusapp.entities;
+
+public class Product {
+    private String productId;
+    private String name;
+    private BigDecimal price;
+    private String brand;
+    private String availability;
+    private String age;
+    private String gender;
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getAvailability() {
+        return availability;
+    }
+
+    public void setAvailability(String availability) {
+        this.availability = availability;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
