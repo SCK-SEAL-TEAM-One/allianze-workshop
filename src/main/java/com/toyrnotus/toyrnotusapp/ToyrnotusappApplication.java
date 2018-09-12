0a55925f8ade29748990e51e7f00ed6ad2b4f508
Amzikan <amzikan@gmail.com>
2018-09-12 11:22:06 +0700

    init project

---
+++ src/main/java/com/toyrnotus/toyrnotusapp/ToyrnotusappApplication.java
@@ -0,0 +1,12 @@
+package com.toyrnotus.toyrnotusapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyrnotusappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyrnotusappApplication.class, args);
+	}
+}
