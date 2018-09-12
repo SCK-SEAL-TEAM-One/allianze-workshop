23106c012f6f85ceed01ae958e8f5c57edd3aa28
maetinee <maetinee.lo@hotmail.com>
2018-09-12 15:13:03 +0700

    [Edited]: ProductServiceTest.java

--- src/test/java/com/toyrnotus/toyrnotusapp/services/ProductServiceTest.java
+++ src/test/java/com/toyrnotus/toyrnotusapp/services/ProductServiceTest.java
@@ -1,27 +1,48 @@
 package com.toyrnotus.toyrnotusapp.services;
 
+import static org.junit.Assert.assertEquals;
+
 import com.toyrnotus.toyrnotusapp.entities.Product;
+import com.toyrnotus.toyrnotusapp.repositories.ProductRepository;
 import com.toyrnotus.toyrnotusapp.services.impl.ProductServiceImpl;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
+import java.util.ArrayList;
 import java.util.List;
-
 
 public class ProductServiceTest {
     @InjectMocks
-    private ProductServiceImpl productService
+    private ProductServiceImpl productService;
 
     @Mock
     private ProductRepository productRepository;
 
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+    }
+
     @Test
     public void searchProduct_with_age_3to5_gender_Female_should_be_return_3_productList(){
+        List<Product> productList= new ArrayList<>();
+        Product product1 = Mockito.mock(Product.class);
+        productList.add(product1);
 
-        List<Product> productList=productService.searchProduct("3_to_5", "Female");
+        Product product2 = Mockito.mock(Product.class);
+        productList.add(product2);
 
+        Product product3 = Mockito.mock(Product.class);
+        productList.add(product3);
+
+        Mockito.doReturn(productList).when(productRepository).findByAgeAndGender("3_to_5", "Female");
+
+        List<Product> result  = productService.searchProduct("3_to_5", "Female");
+        assertEquals(3,result.size());
     }
 
 }
