cf0bdc9567dbc903c7442e4536f663dc28be933c
Amzikan <amzikan@gmail.com>
2018-09-12 14:38:40 +0700

    [Added] ProductRepository interface

---
+++ src/main/java/com/toyrnotus/toyrnotusapp/repositories/ProductRepository.java
@@ -0,0 +1,12 @@
+package com.toyrnotus.toyrnotusapp.repositories;
+
+import com.toyrnotus.toyrnotusapp.entities.Product;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository {
+
+    public List<Product> findByAgeAndGender(String age, String gender);
+}
