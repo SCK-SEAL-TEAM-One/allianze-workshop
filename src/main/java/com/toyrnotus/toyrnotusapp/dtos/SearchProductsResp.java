afb6d20cfcc8e3d71260498a3ba8ff8791b2ff1c
meeboom <natchaya.jaru@gmail.com>
2018-09-12 14:05:37 +0700

    [Created] add SearchProductsResp.java

--- src/main/java/com/toyrnotus/toyrnotusapp/dtos/SearchProductsResp.java
+++ src/main/java/com/toyrnotus/toyrnotusapp/dtos/SearchProductsResp.java
@@ -1,43 +1,16 @@
-package com.toyrnotus.dtos;
+package com.toyrnotus.toyrnotusapp.dtos;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 public class SearchProductsResp {
-    private String name;
-    private BigDecimal price;
-    private String availability;
-    private String brand;
+    private List products;
 
-    public String getName() {
-        return name;
+    public List getProducts() {
+        return products;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
-
-    public String getAvailability() {
-        return availability;
-    }
-
-    public void setAvailability(String availability) {
-        this.availability = availability;
-    }
-
-    public String getBrand() {
-
-        return brand;
-    }
-
-    public void setBrand(String brand) {
-        this.brand = brand;
+    public void setProducts(List products) {
+        this.products = products;
     }
 }
